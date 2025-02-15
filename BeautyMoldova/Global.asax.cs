f31bf6f5b782420e17e6ddd79d75a3efafad5e59
Galina Tolev <galina.tolev@isa.utm.md>
2025-02-15 22:11:48 +0200

    первый коммит

---
+++ BeautyMoldova/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace CosmeticShop
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            try
+            {
+                var authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+                if (authCookie == null) return;
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                if (authTicket == null || authTicket.Expired) return;
+                var roles = authTicket.UserData.Split(',');
+                HttpContext.Current.User = new GenericPrincipal(new FormsIdentity(authTicket), roles);
+            }
+            catch
+            {
+                FormsAuthentication.SignOut();
+                var authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+                if (authCookie == null) return;
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                if (authTicket == null || authTicket.Expired) return;
+                var roles = authTicket.UserData.Split(',');
+                HttpContext.Current.User = new GenericPrincipal(new FormsIdentity(authTicket), roles);
+            }
+        }
+        
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
